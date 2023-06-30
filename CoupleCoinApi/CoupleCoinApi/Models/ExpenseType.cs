56e8d7969b1b2ddfbbd4461ebe1dc33bbc9b9f74
Joao V B Andrade <80553392+vitorbda@users.noreply.github.com>
2023-06-30 19:16:11 -0300

    [ + ] Criação da classe ExpenseType

--- CoupleCoinApi/CoupleCoinApi/Models/ExpenseType.cs
+++ CoupleCoinApi/CoupleCoinApi/Models/ExpenseType.cs
@@ -1,10 +1,20 @@
-﻿namespace CoupleCoinApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoupleCoinApi.Models
 {
     public class ExpenseType
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
         public User? Owner { get; set; }
+        public bool? IsCouple { get; set; }
+        public Couple? Couple { get; set; }
+        public DateTime? AddDate { get; set; }
+        public DateTime? AlterDate { get; set;}
+        [Required]
         public bool IsActive { get; set; }        
     }
 }
